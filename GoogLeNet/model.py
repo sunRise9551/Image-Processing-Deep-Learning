45bb5f72021baa9c4fa3eaae2807b8340b25fe72
sunRise9551 <wongkiesun@gmail.com>
2023-12-13 23:29:13 +0800

    class BasicConv2d

---
+++ GoogLeNet/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.relu(x)
+        return x
+
